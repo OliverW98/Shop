2022bea69f641693426b0bc98cd23dea334a724f
Oliverw98 <oliverwilkes22@gmail.com>
2022-01-08 20:00:24 +0000

    basic vaildation

--- shopapp/src/components/SignupPage.js
+++ shopapp/src/components/SignupPage.js
@@ -1,6 +1,60 @@
+import { useState } from "react";
 function SignupPage() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [conPassword, setConPassword] = useState("");
+
+  const [firstNameErr, setFirstNameErr] = useState("");
+  const [lastNameErr, setLastNamErr] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+
+  function checkSignUpDetails() {
+    let valldSignUp = true;
+    setFirstNameErr("");
+    setLastNamErr("");
+    setEmailErr("");
+    setPasswordErr("");
+    if (firstName === "") {
+      setFirstNameErr("Field cannot be empty");
+      valldSignUp = false;
+    }
+    if (lastName === "") {
+      setLastNamErr("Field cannot be empty");
+      valldSignUp = false;
+    }
+    if (email === "") {
+      setEmailErr("Field cannot be empty");
+      valldSignUp = false;
+    }
+    if (password !== "") {
+      if (password.length > 8) {
+        if (password !== conPassword) {
+          setPasswordErr("Passwords do not match");
+          valldSignUp = false;
+        }
+      } else {
+        setPasswordErr("Passowrd must be longer than 8 characters");
+        valldSignUp = false;
+      }
+    } else {
+      setPasswordErr("Field cannot be empty");
+      valldSignUp = false;
+    }
+    if (valldSignUp) {
+      signUpUser();
+    }
+  }
+
   async function signUpUser() {
-    const data = { username: "example" };
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
     const body = JSON.stringify(data);
     console.log(body);
 
@@ -25,32 +79,54 @@ function SignupPage() {
         <div className="column"></div>
         <div className="column">
           <div className="box">
+            <p className="title has-text-centered">Sign Up</p>
             <div className="field">
               <label className="label">First Name</label>
               <div className="control has-icons-left">
-                <input className="input" type="text" placeholder="First Name" />
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
                 <span className="icon is-small is-left">
                   <i className="fas fa-user"></i>
                 </span>
               </div>
+              <p className="help is-danger">{firstNameErr}</p>
             </div>
             <div className="field">
               <label className="label">Last Name</label>
               <div className="control has-icons-left">
-                <input className="input" type="text" placeholder="Last Name" />
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
                 <span className="icon is-small is-left">
                   <i className="fas fa-user"></i>
                 </span>
               </div>
+              <p className="help is-danger">{lastNameErr}</p>
             </div>
             <div className="field">
               <label className="label">Email</label>
               <div className="control has-icons-left">
-                <input className="input" type="email" placeholder="Email" />
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
                 <span className="icon is-small is-left">
                   <i className="fas fa-envelope"></i>
                 </span>
               </div>
+              <p className="help is-danger">{emailErr}</p>
             </div>
             <div className="field">
               <label className="label">Password</label>
@@ -59,11 +135,14 @@ function SignupPage() {
                   className="input"
                   type="password"
                   placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <span className="icon is-small is-left">
                   <i className="fas fa-lock"></i>
                 </span>
               </div>
+              <p className="help is-danger">{passwordErr}</p>
             </div>
             <div className="field">
               <label className="label">Confirm Password</label>
@@ -72,6 +151,8 @@ function SignupPage() {
                   className="input"
                   type="password"
                   placeholder="Password"
+                  value={conPassword}
+                  onChange={(e) => setConPassword(e.target.value)}
                 />
                 <span className="icon is-small is-left">
                   <i className="fas fa-lock"></i>
@@ -80,7 +161,10 @@ function SignupPage() {
             </div>
             <div className="field">
               <div className="control">
-                <button className="button is-primary" onClick={signUpUser}>
+                <button
+                  className="button is-primary"
+                  onClick={checkSignUpDetails}
+                >
                   Sign Up
                 </button>
               </div>
